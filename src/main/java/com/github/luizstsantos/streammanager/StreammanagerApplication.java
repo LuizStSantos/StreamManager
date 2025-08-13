dd43742288de5f8429cd18e0ec75cdd710f80053
LuizStSantos <luiz.st.santos@gmail.com>
2025-08-13 06:44:28 -0300

    feat: setup inicial do projeto Spring Boot com H2
    - Criado projeto Spring Boot
    - Adicionadas dependências: Spring Web, Spring Data JPA, Lombok, H2 Database
    - Configurar banco H2 em memória para desenvolvimento; closes #1

---
+++ src/main/java/com/github/luizstsantos/streammanager/StreammanagerApplication.java
@@ -0,0 +1,13 @@
+package com.github.luizstsantos.streammanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreammanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreammanagerApplication.class, args);
+	}
+
+}
